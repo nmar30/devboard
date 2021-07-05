d591d6e0262c0e70c66c166de289de1e709e58cd
nmar30 <nmarcisso@live.com>
2021-07-05 17:55:11 -0400

    Updated Dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,17 +1,73 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-import { Spinner } from "react-bootstrap";
+import { Spinner, Row, Col, ListGroup, Badge } from "react-bootstrap";
+import axios from "../axios";
 
-const Dashboard = () => {
+const Dashboard = ({ user }) => {
+  const [taskList, setTaskList] = useState(null);
+  const [projectList, setProjectList] = useState(null);
   const [isLoaded, setLoaded] = useState(false);
+
+  const getUserTasks = async () => {
+    try {
+      const jwt = await JSON.parse(localStorage.getItem("token"));
+      const response = await axios.get(`data/tasks/?user=${user.user_id}`, {
+        headers: { Authorization: "Bearer " + jwt.access },
+      });
+      setTaskList(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const getUserProjects = async () => {
+    try {
+      const jwt = await JSON.parse(localStorage.getItem("token"));
+      const response = await axios.get(`data/projects/?user=${user.user_id}`, {
+        headers: { Authorization: "Bearer " + jwt.access },
+      });
+      setProjectList(response.data);
+      console.log(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   useEffect(() => {
-    setLoaded(true);
+    if (!taskList) {
+      getUserTasks();
+      getUserProjects();
+    } else {
+      setLoaded(true);
+    }
     console.log(isLoaded);
-  }, [isLoaded]);
+  }, [isLoaded, getUserTasks, taskList, getUserProjects, projectList]);
 
   if (isLoaded) {
-    return <h1>Dashboard</h1>;
+    return (
+      <Row>
+        <h1>Dashboard</h1>
+        <Col>
+          <h2>My Tasks</h2>
+          <ListGroup>
+            {taskList.map((i, index) => (
+              <ListGroup.Item key={index}>
+                {i.name} | {i.status}
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        </Col>
+        <Col>
+          <h2>My Projects</h2>
+          <ListGroup>
+            {projectList.map((i, index) => (
+              <ListGroup.Item key={index}>{i.name}</ListGroup.Item>
+            ))}
+          </ListGroup>
+        </Col>
+      </Row>
+    );
   } else {
     return <Spinner animation="border" />;
   }

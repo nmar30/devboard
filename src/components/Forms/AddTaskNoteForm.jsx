414eec1e5d4e7f7c9f89e3b5a4db2d230a8ad3f9
nmar30 <nmarcisso@live.com>
2021-07-07 12:59:54 -0400

    Added Timer on TaskNotes

--- src/components/Forms/AddTaskNoteForm.jsx
+++ src/components/Forms/AddTaskNoteForm.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {
   Form,
   Button,
@@ -13,9 +13,48 @@ import Timer from "../Timer/Timer";
 
 const AddTaskNoteForm = ({ addTaskNote }) => {
   const { values, handleChange, handleSubmit } = useForm(submit);
+  const [second, setSecond] = useState("00");
+  const [minute, setMinute] = useState("00");
+  const [isActive, setIsActive] = useState(false);
+  const [counter, setCounter] = useState(0);
 
   function submit() {
     addTaskNote({ ...values });
+  }
+
+  useEffect(() => {
+    let intervalId;
+
+    if (isActive) {
+      intervalId = setInterval(() => {
+        const secondCounter = counter % 60;
+        const minuteCounter = Math.floor(counter / 60);
+
+        const computedSecond =
+          String(secondCounter).length === 1
+            ? `0${secondCounter}`
+            : secondCounter;
+        const computedMinute =
+          String(minuteCounter).length === 1
+            ? `0${minuteCounter}`
+            : minuteCounter;
+
+        setSecond(computedSecond);
+        setMinute(computedMinute);
+        values.time_worked = computedMinute;
+        setCounter((counter) => counter + 1);
+      }, 1000);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [isActive, counter]);
+
+  function resetTimer() {
+    setIsActive(false);
+    setCounter(0);
+    setSecond("00");
+    setMinute("00");
+    values.time_worked = 0;
   }
 
   return (
@@ -53,20 +92,34 @@ const AddTaskNoteForm = ({ addTaskNote }) => {
           value={values.date_worked}
         />
       </InputGroup>
+      {isActive && (
+        <h3>
+          Elapsed Time: {minute}:{second}
+        </h3>
+      )}
       <InputGroup className="mb-1">
         <InputGroup.Prepend>
           <InputGroup.Text>Time Worked:</InputGroup.Text>
         </InputGroup.Prepend>
         <FormControl
+          type="number"
           placeholder="Enter Time"
+          name="time_worked"
           aria-label="Enter Time"
           aria-describedby="basic-addon2"
           onChange={handleChange}
           value={values.time_worked}
         />
         <InputGroup.Append>
-          <Button variant="outline-secondary">Start</Button>
-          <Button variant="outline-secondary">Reset</Button>
+          <Button
+            variant="outline-secondary"
+            onClick={() => setIsActive(!isActive)}
+          >
+            {isActive ? "Pause" : "Start"}
+          </Button>
+          <Button variant="outline-secondary" onClick={() => resetTimer()}>
+            Reset
+          </Button>
         </InputGroup.Append>
       </InputGroup>
       <Button variant="primary" type="submit">
